2cf2936a7f5f9f1f90a1595e260afebe1060f3b5
Das10177 <sdas92211@gmail.com>
2025-01-18 20:36:03 +0530

    Tricker test not make

---
+++ First-page/src/Components/News_Tricker.jsx
@@ -0,0 +1,87 @@
+import { ReactTicker } from "@guna81/react-ticker";
+import "./styles.css";
+
+export default function News_Tricker() {
+  const data = [
+    {
+      id: 1,
+      value:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      id: 1,
+      value:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      id: 1,
+      value:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      id: 1,
+      value:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+  ];
+
+  const renderItem = (item) => {
+    return (
+      <p
+        style={{
+          whiteSpace: "nowrap",
+          color: "orange",
+        }}
+      >
+        {item.value}
+      </p>
+    );
+  };
+
+  return (
+    <div className="App">
+      {/* using default ticker item component */}
+      <div>
+        <ReactTicker
+          data={data.map((item) => item.value)}
+          speed={20}
+          keyName="_id"
+          tickerStyle={{
+            position: "fixed",
+
+            bottom: 0,
+            left: "0",
+            width: "100%",
+            height: "40px",
+            backgroundColor: "#fff",
+            zIndex: 99,
+            borderTop: "1px solid #e0e0e0",
+            border: "red",
+          }}
+          tickerClassName="news-ticker"
+        />
+      </div>
+
+      <h1 style={{ textAlign: "center" }}>React Ticker Examble</h1>
+
+      {/* using custom ticker item component */}
+      <ReactTicker
+        data={data}
+        component={renderItem}
+        speed={40}
+        keyName="_id"
+        tickerStyle={{
+          position: "fixed",
+          bottom: 25,
+          left: "0",
+          width: "100%",
+          height: "40px",
+          backgroundColor: "#fff",
+          zIndex: 99,
+          borderTop: "1px solid #e0e0e0",
+        }}
+        tickerClassName="news-ticker"
+      />
+    </div>
+  );
+}
--- First-page/src/Components/Tricker.jsx
+++ First-page/src/Components/Tricker.jsx
@@ -1,83 +1,33 @@
-import { ReactTicker } from "@guna81/react-ticker";
+import './Tricker.css';
 
-
-export default function App() {
-  const data = [
-    {
-      id: 1,
-      value:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    },
-    {
-      id: 1,
-      value:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    },
-    {
-      id: 1,
-      value:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    },
-    {
-      id: 1,
-      value:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    },
-  ];
-
-  const renderItem = (item) => {
-    return (
-      <p
-        style={{
-          whiteSpace: "nowrap",
-          color: "orange",
-        }}
-      >
-        {item.value}
-      </p>
-    );
-  };
-
+const NewsContainer = () => {
   return (
-    <div className="App">
-      {/* using default ticker item component */}
-      <ReactTicker
-        data={data.map((item) => item.value)}
-        speed={20}
-        keyName="_id"
-        tickerStyle={{
-          position: "fixed",
-          top: 0,
-          left: "0",
-          width: "100%",
-          height: "40px",
-          backgroundColor: "#fff",
-          zIndex: 99,
-          borderTop: "1px solid #e0e0e0",
+    <div className="news-container">
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "red",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          padding: "0 20px",
+          color: "white",
+          fontWeight: "bold",
+          zIndex: "200",
         }}
-        tickerClassName="news-ticker"
-      />
-
-      <h1 style={{ textAlign: "center" }}>React Ticker Examble</h1>
-
-      {/* using custom ticker item component */}
-      <ReactTicker
-        data={data}
-        component={renderItem}
-        speed={40}
-        keyName="_id"
-        tickerStyle={{
-          position: "fixed",
-          bottom: 0,
-          left: "0",
-          width: "100%",
-          height: "40px",
-          backgroundColor: "#fff",
-          zIndex: 99,
-          borderTop: "1px solid #e0e0e0",
-        }}
-        tickerClassName="news-ticker"
-      />
+      > Breaking News
+      </div>
+      <ul>
+        <li>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam!
+        </li>
+        <li>Lorem ipsum dolor sit</li>
+        <li>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam!
+        </li>
+      </ul>
     </div>
   );
-}
+};
+
+export default NewsContainer;
